7901e74dd02efb85da9330e5408c0c7ddcf109bb
sadeeva <k.sadeeva@gmail.com>
2018-03-15 16:05:33 -0400

    Задание 4- Создание контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String lastname;
+    private final String company;
+    private final String address;
+    private final String mobile;
+    private final String email;
+
+    public ContactData(String firstname, String lastname, String company, String address, String mobile, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.company = company;
+        this.address = address;
+        this.mobile = mobile;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
