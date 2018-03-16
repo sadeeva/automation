5a4b96c0a248a3fd76128e069c260698973502c0
sadeeva <k.sadeeva@gmail.com>
2018-03-16 18:30:32 -0400

    Added GroupModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -38,4 +38,11 @@ public class GroupHelper extends HelperBase {
         }
     }
 
+    public void initGroupModification() {
+        click(By.name("edit"));
+    }
+
+    public void submitGroupModification() {
+        click(By.name("update"));
+    }
 }
