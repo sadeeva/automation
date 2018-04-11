58786fefefb99e359f6c2e01d3cecb1d98f2e4ae
sadeeva <k.sadeeva@gmail.com>
2018-04-11 19:34:11 -0400

    4.8 Groups only

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,31 +3,26 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-    private final String id;
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                '}';
-    }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
+
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
+
         this.footer = footer;
     }
 
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
@@ -46,12 +41,13 @@ public class GroupData {
         return footer;
     }
 
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(id, groupData.id) &&
+        return id == groupData.id &&
                 Objects.equals(name, groupData.name);
     }
 
@@ -59,5 +55,18 @@ public class GroupData {
     public int hashCode() {
 
         return Objects.hash(id, name);
+    }
+
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
